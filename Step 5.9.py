2ff429aca402f4e0daf936ff1094f7f0beee6f03
Maria Korobeynikova <siverka@gmail.com>
2017-02-11 18:20:53 +0300

    resolved task 5.9, 5.19

---
+++ Step 5.9.py
@@ -0,0 +1,11 @@
+a, b = (int(i) for i in input().split())
+
+for n in range(a, b+1):
+    if n % 15 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
