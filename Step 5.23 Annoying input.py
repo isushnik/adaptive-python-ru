47ee9b9724746db2067daa857970bee34bfeaeca
Maria Korobeynikova <siverka@gmail.com>
2017-02-18 15:24:11 +0300

    resolved step 5.23 Annoying input

---
+++ Step 5.23 Annoying input.py
@@ -0,0 +1,13 @@
+def get_int(start_message, error_message, end_message):
+    print(start_message)
+    while True:
+        try:
+            x = int(input())
+            print(end_message)
+            return x
+        except ValueError:
+            print(error_message)
+
+
+print(get_int('Input int number:', 'Wrong value. Input int number:', 'Thank you.'))
+
