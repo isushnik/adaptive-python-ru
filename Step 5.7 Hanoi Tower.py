1e84fb25b0c00d3f718a996b4547432421889666
Maria Korobeynikova <siverka@gmail.com>
2017-03-09 14:44:17 +0300

    updated move() function of Hanoi Tower

--- Step 5.7 Hanoi Tower.py
+++ Step 5.7 Hanoi Tower.py
@@ -1,31 +1,29 @@
-def move(i: int, j: int):
-    if len(pigs[i]) == 0:
-        pigs[i].append(pigs[j].pop())
-        print(j+1, '-', i+1)
-    elif len(pigs[j]) == 0:
-        pigs[j].append(pigs[i].pop())
-        print(i+1, '-', j+1)
-    elif pigs[i][-1] > pigs[j][-1]:
-        pigs[i].append(pigs[j].pop())
-        print(j+1, '-', i+1)
-    elif pigs[j][-1] > pigs[i][-1]:
-        pigs[j].append(pigs[i].pop())
-        print(i+1, '-', j+1)
+def move(indxs: list):
+    lens = [len(pegs[i]) == 0 for i in indxs]
+    if any(lens):
+        l = indxs[lens.index(True)]  # 0
+        k = indxs[lens.index(False)]  # not 0
+    else:
+        vals = [pegs[i][-1] for i in indxs]
+        l = [i for i in indxs if pegs[i][-1] == max(vals)][0]
+        k = [i for i in indxs if pegs[i][-1] == min(vals)][0]
+    pegs[l].append(pegs[k].pop())
+    print(k+1, '-', l+1)
 
 
-# n = int(input())
-n = 3
-pigs = [[i for i in range(n, 0, -1)], [], []]
+n = int(input())
+# n = 3
+pegs = [[i for i in range(n, 0, -1)], [], []]
+print(pegs)
 
 if n % 2 == 0:
     steps = [[0, 1], [0, 2], [1, 2]]
 else:
     steps = [[0, 2], [0, 1], [1, 2]]
 
-print(pigs)
-while len(pigs[-1]) != n:
+while len(pegs[-1]) != n:
     for s in steps:
-        if len(pigs[-1]) != n:
-            move(*s)
+        if len(pegs[-1]) != n:
+            move(s)
 
-print(pigs)
+print(pegs)
