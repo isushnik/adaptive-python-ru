71ba845a3db726c6aa52857e65633cf7a60ff975
Maria Korobeynikova <siverka@gmail.com>
2017-05-08 23:23:41 +0300

    resolved step 5.2 Old LCD calculator

--- Step 5.2 Old LCD calculator.py
+++ Step 5.2 Old LCD calculator.py
@@ -4,13 +4,26 @@ a3 = '    '
 a4 = '   |'
 a5 = '|   '
 
-d = {0: [a1, a2, a2, a3, a2, a2, a1],
-     1: [a3, a4, a4, a3, a4, a4, a3],
-     2: [a1, a4, a4, a1, a5, a5, a1],
-     3: [a1, a4, a4, a1, a4, a4, a1],
-     4: [a3, a2, a2, a1, a4, a4, a3],
-     5: [a1, a5, a5, a1, a4, a4, a1],
-     6: [a1, a5, a5, a1, a2, a2, a1],
-     7: [a1, a4, a4, a3, a4, a4, a3],
-     8: [a1, a2, a2, a1, a2, a2, a1],
-     9: [a1, a2, a2, a1, a4, a4, a1]}
+d = {'0': [a1, a2, a2, a3, a2, a2, a1],
+     '1': [a3, a4, a4, a3, a4, a4, a3],
+     '2': [a1, a4, a4, a1, a5, a5, a1],
+     '3': [a1, a4, a4, a1, a4, a4, a1],
+     '4': [a3, a2, a2, a1, a4, a4, a3],
+     '5': [a1, a5, a5, a1, a4, a4, a1],
+     '6': [a1, a5, a5, a1, a2, a2, a1],
+     '7': [a1, a4, a4, a3, a4, a4, a3],
+     '8': [a1, a2, a2, a1, a2, a2, a1],
+     '9': [a1, a2, a2, a1, a4, a4, a1]}
+
+number = input()
+# number = '0123456789'
+n = len(number)
+matrix = [[d[s][i] for s in number] for i in range(7)]
+frame = '|'
+
+print('x' + '-' * (5 * n - 1) + 'x')
+for i in range(7):
+    print(frame, end='')
+    print(*matrix[i], end='')
+    print(frame)
+print('x' + '-' * (5 * n - 1) + 'x')
