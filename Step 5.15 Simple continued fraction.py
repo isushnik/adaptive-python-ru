9d764eb4f2e1426e2e63e18b3ddad4e21bbd8fda
Maria Korobeynikova <siverka@gmail.com>
2017-06-03 10:13:24 +0300

    resolved step 15 Simple continued fraction

--- Step 5.15 Simple continued fraction.py
+++ Step 5.15 Simple continued fraction.py
@@ -1,17 +1,9 @@
-# x, y = map(int, input().split('/'))
-# print(x, y)
-m, n = 239, 30
-
-fractions = []
-a = int(x/y)
-x = x - y * a
-while int(y/x) != 0:
-    print('a', a)
-    print('x', x)
-    print('y', y)
-    fractions.append(a)
-    a = int(y/x)
-    y = y - a * x
-
-print(fractions)
-
+m, n = map(int, input().split('/'))
+i = int(m/n)
+fractions = [i]
+while m % n != 0:
+    m = m - n * i
+    m, n = n, m
+    i = int(m/n)
+    fractions.append(i)
+print(*fractions)
